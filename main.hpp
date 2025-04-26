e42e5c0d1284a0d421c194a9665e11ae895541a3
jay <jayly.2506@gmail.com>
2025-04-25 19:26:25 -0700

    first

--- main.hpp
+++ main.hpp
@@ -6,3 +6,41 @@
 // ******************************
 // Implement all your functions here
 // ******************************
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void getTwoValues(int &begin, int &end);
+int getNextPrime(int start);
+int getPrevPrime(int end);
+bool isPrime(int n);
+
+bool isPrime(int n){
+    if (n<2){
+        return false;
+    }
+    for (int i=2; i<sqrt(n);i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+}
+
+void getTwoValues(int &begin, int &end){
+    do{
+        cout <<"Enter 2 intergers";
+        cin >> begin >> end;
+    }
+    while (begin >= end);
+}
+
+int getPrevPrime(int end){
+    int n = (end - 1);
+    while (n>1){
+        if (isPrime(n)){
+            return n;
+            --n;
+        }
+    }
+    return -1;
+}
